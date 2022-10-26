bad84a1c39bdc9740236ab06a1da969058e54ad0
손종국 <kyky7852@naver.com>
2022-10-26 18:01:30 +0900

    질문 답변 매칭하기위해 키 값 생성  AI 월별 쿼터 있어서 Token 다 사용시 이용 못한다는 채팅 출력

--- src/main/webapp/resources/js/api.js
+++ src/main/webapp/resources/js/api.js
@@ -64,10 +64,7 @@
 		var userid = $("#mid").val(); // 메시지 정보
 		var channel_name = $("#channel").val(); // 메시지 정보
 		
-		var msg = " 정보:거주지 서울, 나이 20대, 성별 여자, 말투 반말 정보를 바탕으로 질문에 답하세요. Q : "+text+ "A :";	
-		
-					
-		
+		var msg = " 정보:거주지 서울, 나이 20대, 성별 여자, 말투 반말 정보를 바탕으로 질문에 답하세요. Q : "+text+ "A :";			
 			$.ajax({
 		 		 type : 'GET',
 		 	    url :'/talkBot',
@@ -79,27 +76,35 @@
 		    	},
 		 	    dataType : "json",
 
-		 	    success : function(data) {
+		 	    success : function(answer) {
+					var data =JSON.parse(answer[0]); // Bot 답변
+					var user_no = answer[1]; // Bot 답변과 유저 질문을 매칭 시키기 위한 고유번호 
+			    	if(data.msg!=null){ // Token 떨어지면 response로 msg가 날라옴 
+				  $('input[name=alram1]').attr('value',"지금은 운영시간이 아닙니다.");
+		 	    	botsend();		 	    			
+		 	       $("#msg").removeAttr("readonly");       // readonly 삭제
+		 	       $("#msg").removeAttr('placeholder'); // placeholder 삭제
+				} else { 
 		 	    	var prompt_tokens = data.usage.prompt_tokens;
-		 	    	var generated_tokens = data.usage.generated_tokens;
-		 	    	
+		 	    	var generated_tokens = data.usage.generated_tokens;	 	    	
 		 	    	var str = data.generations[0].text;
 		 	    	var words = str.indexOf(':'); // 첫번째로 Q가 있는 위치 index 리턴 		  	    	
 		 	    	var result = str.slice(0, words-2); //0부터 첫번째 Q가 있는위치까지 짜름
 		 	    	$('input[name=alram1]').attr('value',result);
 		 	    	botsend();
-		 	    	talkBotSave(result,prompt_tokens,generated_tokens);		
+		 	    	talkBotSave(result,prompt_tokens,generated_tokens,user_no);		
 		 	       $("#msg").removeAttr("readonly");       // readonly 삭제
 		 	      $("#msg").removeAttr('placeholder');
+		 	      }
 		 	    },
 		 	    error : function(error) {
 		 	        alert(error);    	 	        
 		 	    }
 		 		}); 
 	}
-		function talkBotSave(result,prompt_tokens,generated_tokens){
-			var userid = $("#mid").val(); // 메시지 정보
-			 console.log(result,prompt_tokens,generated_tokens);							
+		function talkBotSave(result,prompt_tokens,generated_tokens,user_no){
+			var userid = $("#mid").val(); // 유저 정보
+			 console.log(result,prompt_tokens,generated_tokens,user_no);							
 					$.ajax({
 				 		 type : 'GET',
 				 	    url :'/talkBotSave',
@@ -107,7 +112,8 @@
 				 	   		userid : userid ,
 				 	   		result : result ,
 				 	   		prompt_tokens : prompt_tokens ,
-				 	  		generated_tokens : generated_tokens  				 	   
+				 	  		generated_tokens : generated_tokens ,
+				 	  		user_no : user_no 				 	   
 				    	},
 				 	    dataType : "json"
 				 		}); 		
