d88dde3321f331d3afbb561891c3796ff0288e3b
손종국 <thswhdrnr12@neonexsoft.com>
2022-11-10 11:21:57 +0900

    Maven -> gradle 로 변환 및 쿼리 JPA 로 변환 작업

---
+++ src/main/java/com/example/toy/jpa/service/JpaAdminService.java
@@ -0,0 +1,27 @@
+package com.example.toy.jpa.service;
+
+import com.example.toy.jpa.entity.Login_User;
+import com.example.toy.jpa.repository.JpaAdminRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+
+@RequiredArgsConstructor // final이 붙거나 @NotNull 이 붙은 필드의 생성자를 자동 생성해주는 롬복 어노테이션
+@Service
+public class JpaAdminService {
+    private final JpaAdminRepository jpaAdminRepository;
+
+    public List<Login_User> ShowUser(){
+
+     return jpaAdminRepository.findAll();
+
+    }
+    public List<Login_User> uerIncreament(){
+
+        return jpaAdminRepository.findAll();
+
+    }
+
+    }
