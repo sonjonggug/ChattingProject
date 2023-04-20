1e04399b9a535340618b6a1904a9993639a8e86e
손종국 <kyky7852@naver.com>
2023-04-20 10:31:16 +0900

    Revert "시큐리티 로그인 시 튕겨나오는 오류 수정"
    This reverts commit 2b055f3377db82053d0892b4e0449758220f1278.

--- src/main/java/com/example/toy/config/SecurityConfig.java
+++ src/main/java/com/example/toy/config/SecurityConfig.java
@@ -2,7 +2,6 @@ package com.example.toy.config;
 
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -16,12 +15,7 @@ import lombok.RequiredArgsConstructor;
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     private final UserService userService;
 
-    @Override
-    public void configure(WebSecurity web) throws Exception {
-        web
-                .ignoring().antMatchers( "/css/**", "/js/**", "/img/**");
-    }
-    
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http    .csrf().disable()
